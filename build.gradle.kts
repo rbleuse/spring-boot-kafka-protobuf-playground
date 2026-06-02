ba8e5ab9e19b9e02ecf52ac9fe645e10c357569a
Rémi Bleuse <remi.bleuse@gmail.com>
2026-06-02 11:12:31 +0900

    build: add protobuf and kafka dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "2.3.21"
 	id("org.springframework.boot") version "4.1.0-RC1"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("com.google.protobuf") version "0.10.0"
 }
 
 group = "io.github.rbleuse"
@@ -19,11 +20,32 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.kafka:spring-kafka")
+	implementation("com.google.protobuf:protobuf-java:4.35.0")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testImplementation("org.springframework.kafka:spring-kafka-test")
+	testImplementation("org.testcontainers:testcontainers-kafka")
+	testImplementation("io.kotest:kotest-assertions-core-jvm:6.1.0")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+protobuf {
+	protoc {
+		artifact = "com.google.protobuf:protoc:4.35.0"
+	}
+	plugins {
+		remove(getByName("grpc"))
+	}
+	generateProtoTasks {
+		all().configureEach {
+			plugins {
+				remove(getByName("grpc"))
+			}
+		}
+	}
 }
 
 kotlin {
